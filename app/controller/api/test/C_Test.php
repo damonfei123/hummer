39699a5c01fa24a49a49db6330392fa45d42b2fc
zhangyinfei <zhangyinfei@baidu.com>
2015-01-23 18:21:47 +0800

    add api

--- app/controller/api/test/C_Test.php
+++ app/controller/api/test/C_Test.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\controller\api\test;
 
-class C_Test {
+use App\system\controller\Api_Base;
+
+class C_Test extends Api_Base{
 
     public function get()
     {
-        echo 1;
+        //$this->success(0,'哈哈',array(1,2,3));
+        //$this->error(-1,'哈哈',array(1,2,3));
     }
 }
 
