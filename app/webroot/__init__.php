f4ed62beae13b20dafdcc9778324d94bda95358e
zhangyinfei <zhangyinfei@baidu.com>
2015-01-25 15:15:06 +0800

    upgrade hummer

--- app/webroot/__init__.php
+++ app/webroot/__init__.php
@@ -1,17 +1,29 @@
 <?php
+/**
+ *  所有模块挂在Context下面，使用CTX方法可以获取module.php里定义的模块
+ **/
 function CTX(){
     return end($GLOBALS['__SELF__CONTEXT']);
 }
 
+/**
+ *  快速打印Log的方式
+ **/
 function L($sMsg,$sLevel = 'info')
 {
     call_user_func(array(CTX()->Log, $sLevel), $sMsg);
 }
 
+/**
+ *  获取DB(PDO)
+ **/
 function DB() {
     return CTX()->RDB;
 }
 
+/**
+ *  获取Config模块
+ **/
 function CFG()
 {
     return CTX()->Config;
@@ -54,11 +66,17 @@ function View()
     return CTX()->Template;
 }
 
+/**
+ *  判断DB查询的是否为空
+ **/
 function MEmpty($Model)
 {
     return CTX()->RDB->isModelDataEmpty($Model);
 }
 
+/**
+ *  测试打印用的方法
+ **/
 function pr($mVar) {
     if (php_sapi_name() != 'cli') { echo "<pre>"; }
     if (is_object($mVar)) {
