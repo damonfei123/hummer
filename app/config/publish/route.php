75b9184fd74c66ed79c6ebeebd8e830765cb7422
zhangyinfei <zhangyinfei@baidu.com>
2015-01-26 19:55:20 +0800

    自定义路由

--- app/config/publish/route.php
+++ app/config/publish/route.php
@@ -1,17 +1,52 @@
 <?php
+use Hummer\Component\Helper\Arr;
+use Hummer\Component\Helper\Helper;
+use Hummer\Component\Route\CallBack;
+use Hummer\Component\Http\HttpRequest;
+
 return array(
     'http' => array(
-        '#index.php|dev.php#' => array(
-            array('Hummer\Component\Route\Mode', 'Http_Page'),
-            'App\\controller\\web\\',
-            'C_',
-            'action'
-        ),
         '#api.php#' => array(
             array('Hummer\Component\Route\Mode', 'Http_Page'),
             'App\\controller\\api\\',
             'C_',
             ''
+        ),
+        //自定义路由
+        '#index.php#' => array(
+            function(
+                $REQ,
+                $RES,
+                $sControllerPath,
+                $sControllerPre,
+                $sActionPre,
+                $HitMode,
+                array $aDefaultCA = array('main', 'default')
+            ){
+                $sControllerPathPre = sprintf('%s%s%s%s%s',
+                    $sControllerPath,
+                    $REQ->getG('m'),
+                    '\\',
+                    $sControllerPre,
+                    ucfirst($REQ->getG('c'))
+                );
+                HttpRequest::$FORGE_REQUEST_URI = sprintf('/%s/%s/%s',
+                    $REQ->getG('m'), $REQ->getG('c'), $REQ->getG('a'));
+                $sAction = sprintf('%s%s', $sActionPre, ucfirst($REQ->getG('a')));
+                $CallBack = new CallBack();
+                $CallBack->setCBObject($sControllerPathPre, $sAction);
+
+                return $CallBack;
+            },
+            'App\\controller\\web\\',
+            'C_',
+            'action'
+        ),
+        array(
+            array('Hummer\Component\Route\Mode', 'Http_Page'),
+            'App\\controller\\web\\',
+            'C_',
+            'action'
         ),
     ),
     'cli' => array(
